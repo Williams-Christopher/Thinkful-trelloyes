719f094d7ab001872957fdc76039daca456148b1
Christopher Williams <cw@chriswillia.ms>
2019-04-29 22:31:13 -0500

    Adds import for STORE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.css';
 
-function App() {
+function App(props) {
+  console.log(props.store);
   return (
     <main className='App'>
       {/* content goes here */}
+      <header className='App-header'>
+        <h1>Trelloeyes!</h1>
+      </header>
+      <div className='App-list'>
+      </div>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import './store';
+
+import STORE from './store';
 
 ReactDOM.render(
-    <App>
+    <App store={STORE}>
     </App>,
     document.getElementById('root'));
